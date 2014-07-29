69472dfd0ffca80bf7ddfea91168838688dffecc
spch2008 <spch2008@foxmail.com>
2014-07-28 18:01:47 -0700

    change file name

---
+++ sl_server/sl_server_register.h
@@ -0,0 +1,54 @@
+#ifndef _SLSERVER_REGISTER_H_
+#define _SLSERVER_REGISTER_H_
+
+#include "sl_server_core.h"
+#include "sl_circle_queue.h"
+
+#include <stdio.h>
+
+typedef void *(*thread_func)(void *);
+
+struct _sl_server_type_pool_t
+{
+    thread_func master_handler;
+    thread_func worker_handler;
+};
+
+
+typedef struct _sl_server_type_pool_t sl_server_type_pool_t;
+
+void *a(void *p)
+{
+    printf("Hello");
+}
+
+void *b(void *p)
+{
+    printf("World!\n");
+}
+
+typedef int (*init_func)(int);
+typedef int (*put_func)(int);
+typedef int (*get_func)(int*);
+
+struct _sl_server_queue_t
+{
+    init_func init;
+    get_func  get;
+    put_func  put;
+};
+
+typedef struct _sl_server_queue_t sl_server_queue_t;
+
+sl_server_queue_t sl_queue[] = 
+{
+    {init_circle_queue, get_circle_event, put_circle_event} 
+};
+
+sl_server_type_pool_t g_server_type_pool [] = 
+{
+    {a, b}
+};
+
+
+#endif
