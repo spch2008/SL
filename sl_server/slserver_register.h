bf2389bb4cf289449b7703260e074ba472b57210
spch2008 <spch2008@foxmail.com>
2014-07-28 17:54:40 -0700

    change slserver_register ncompatible pointer

--- sl_server/slserver_register.h
+++ sl_server/slserver_register.h
@@ -2,6 +2,7 @@
 #define _SLSERVER_REGISTER_H_
 
 #include "slserver_core.h"
+#include "sl_circle_queue.h"
 
 #include <stdio.h>
 
@@ -25,6 +26,24 @@ void *b(void *p)
 {
     printf("World!\n");
 }
+
+typedef int (*init_func)(int);
+typedef int (*put_func)(int);
+typedef int (*get_func)(int*);
+
+struct _sl_server_queue_t
+{
+    init_func init;
+    get_func  get;
+    put_func  put;
+};
+
+typedef struct _sl_server_queue_t sl_server_queue_t;
+
+sl_server_queue_t sl_queue[] = 
+{
+    {init_circle_queue, get_circle_event, put_circle_event} 
+};
 
 sl_server_type_pool_t g_server_type_pool [] = 
 {
