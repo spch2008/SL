9fb350e265b7d7164f16631737eaf660514e917e
spch2008 <spch2008@foxmail.com>
2014-08-10 19:32:53 -0700

    add sl_io

---
+++ sl_io/sl_io.h
@@ -0,0 +1,19 @@
+#ifndef _SL_IO_H_
+#define _SL_IO_H_
+
+struct _sl_io_head_t
+{
+    int id;
+    int version;
+
+    int reserved;
+    int body_len;
+};
+typedef struct _sl_io_head_t sl_io_head_t;
+
+int sl_io_read(int sock, sl_io_head_t *head);
+int sl_io_write(int sock, sl_io_head_t *head);
+
+
+
+#endif
